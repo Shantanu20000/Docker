17ea74df8160306a4e14a18cdd10af834139af89
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-04-24 17:07:09 +0530

    Update student-rds-table.sql

--- Mysql/student-rds-table.sql.sql
+++ Mysql/student-rds-table.sql.sql
@@ -3,9 +3,9 @@ use studentapp;
 CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,
 	student_name VARCHAR(100) NOT NULL,
     student_addr VARCHAR(100) NOT NULL,
-	student_age VARCHAR(3) NOT NULL,
-	student_qual VARCHAR(20) NOT NULL,
-	student_percent VARCHAR(10) NOT NULL,
-	student_year_passed VARCHAR(10) NOT NULL,
+	student_age VARCHAR(100) NOT NULL,
+	student_qual VARCHAR(100) NOT NULL,
+	student_percent VARCHAR(100) NOT NULL,
+	student_year_passed VARCHAR(100) NOT NULL,
 	PRIMARY KEY (student_id)
 );
